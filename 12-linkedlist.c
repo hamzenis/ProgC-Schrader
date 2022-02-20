ff630fe45c30277cd9aee739abd1a4217e1d4f6d
hamzoalbo <84898985+hamzoalbo@users.noreply.github.com>
2022-02-20 23:33:09 +0100

    Update 12-linkedlist.c

--- 12-linkedlist.c
+++ 12-linkedlist.c
@@ -1,4 +1,25 @@
-/* 12-linkedlist */
+/*
+ * File: 12-linkedlist.c
+ * Project: Einführung in C - Schrader
+ * Created Date: 2022-02-01, 3:38:26 pm
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-02-20, 11:32:30 pm
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022 
+ * 
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-02-20, 11:32:10 pm	H.K.	Neue Funktion
+ * 2022-02-01, 3:38:26 pm	H.K.	-start-
+ */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>

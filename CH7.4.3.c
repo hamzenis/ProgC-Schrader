ec98f09c36c2f67ff2b7000e27304b64d5999875
hamzoalbo <84898985+hamzoalbo@users.noreply.github.com>
2022-02-23 12:55:31 +0100

    added comments

--- CH7.4.3.c
+++ CH7.4.3.c
@@ -5,7 +5,7 @@
  * Author: Hamzenis Kryeziu
  * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
  * -----
- * Last Modified: 2022-02-20, 7:48:02 pm
+ * Last Modified: 2022-02-23, 12:54:58 pm
  * Modified By: Hamzenis Kryeziu
  * -----
  * Copyright (c) 2022
@@ -15,6 +15,7 @@
  * HISTORY:
  * Date              		By		Comments
  * ------------------		----	----------------------------------------------------------
+ * 2022-02-23, 12:54:39 pm	H.K.	added comments
  * 2022-01-02, 5:08:49 pm	H.K.	-start-
  */
 
@@ -22,12 +23,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Größe des Arrays
+// Größe des Arrays
 #define ANZAHL 10
 
+/*
+*  Eine verschachtelte For-Schleife, die das Array durchläuft.
+*  Die äußere For-Schleife i läuft das Array 1-mal durch, währrend das Array
+*  von der inneren For-Schleife j komplett durchgegangen wird, so oft
+*  bis die äußere For-Schleife ihr Limit erreicht hat.
+*/
 void sort(int ieingaben[]) {
     for (int i = 0; i < ANZAHL; i++) {
         for (int j = i; j < ANZAHL; j++) {
+            /*
+            *  Wenn die aktuelle Zahl an der Stelle i größer sein sollte als die
+            *  aktuelle Zahl an der Stelle j, wird die Zahl in j in einer temporären
+            *  Variable temp zwischengespeichert, i tauscht den Platz mit j und
+            *  an der Stelle wo i war wird die zwischengespeicherte Zahl in temp 
+            *  gesichert.
+            */
             if (ieingaben[i] > ieingaben[j]) {
                 int temp = ieingaben[j];
                 ieingaben[j] = ieingaben[i];
@@ -37,13 +51,18 @@ void sort(int ieingaben[]) {
     }
 }
 
-
+/*
+*   Funktion zum printen der Eingabewerte
+*/
 void printEingaben(int ieingaben[]) {
     for (int i = 0; i < ANZAHL; i++) {
         printf("%d.Eingabe %d \n", i + 1, ieingaben[i]);
     }
 }
 
+/*
+*   Funktion zum printen der sortierten Werte 
+*/
 void printSort(int ieingaben[]) {
     for (int i = 0; i < ANZAHL; i++) {
         printf("%d.Sortiert %d \n", i + 1, ieingaben[i]);
@@ -56,7 +75,6 @@ int main() {
     //Werte einlesen
     for (i = 0; i < ANZAHL; i++) {
         printf("%d.Element: ", i + 1);
-
         scanf("%d", &ieingaben[i]);
     }
 

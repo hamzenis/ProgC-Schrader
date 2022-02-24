f95dff499d474a494d44e61d84f371c042a56ad8
hamzoalbo <84898985+hamzoalbo@users.noreply.github.com>
2022-02-24 02:18:34 +0100

    Update CH9.6.3.c

--- CH9.6.3.c
+++ CH9.6.3.c
@@ -5,7 +5,7 @@
  * Author: Hamzenis Kryeziu
  * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
  * -----
- * Last Modified: 2022-02-24, 1:34:52 am
+ * Last Modified: 2022-02-24, 2:18:22 am
  * Modified By: Hamzenis Kryeziu
  * -----
  * Copyright (c) 2022
@@ -18,40 +18,52 @@
  * 2022-01-24, 2:08:17 am	H.K.	-start-
  */
 
+// noch nicht fertig
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+// Anzahl und Länge der Variablen.
+#define STRINGS 6
 #define MAX 10
 
 int main() {
 
-    const char* ptrstrNamen[MAX];
+    const char* ptrstrNamen[STRINGS];
     int cmpVal;
     char temp[MAX];
 
+    /*
+    *  Variable damit die Länge des Arrays errechnet. 
+    */
+    int arrSize = sizeof(ptrstrNamen) / sizeof(ptrstrNamen[0]);
+
+    // Variablen Values
     ptrstrNamen[0] = "Peter";
     ptrstrNamen[1] = "Paul";
     ptrstrNamen[2] = "Mary";
     ptrstrNamen[3] = "Puff";
     ptrstrNamen[4] = "Magic";
     ptrstrNamen[5] = "Dragon";
-
-    for (int i = 0; i < MAX; i++) {
+  
+    // 
+    for (int i = 0; i < arrSize - 1; i++) {
         cmpVal = strcmp(ptrstrNamen[i], ptrstrNamen[i + 1]);
+        printf("%d\n", cmpVal);
         if (cmpVal > 0) {
-            strcpy(temp, ptrstrNamen[i]);
+            strcpy(temp, ptrstrNamen[i+1]);
             ptrstrNamen[i + 1] = ptrstrNamen[i];
             ptrstrNamen[i] = temp;
         }
-
     }
 
-    for (int i = 0; i < MAX; i++) {
-        printf("%s\n", ptrstrNamen[i]);
-    }
+    // Print Funktion
+    for (int i = 0; i < arrSize; i++) {
+        printf("%d.%s\n", i+1, ptrstrNamen[i]);
 
+    }
 
     return 0;
 }

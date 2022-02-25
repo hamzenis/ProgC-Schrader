3ebe3dc3378fa6348daacc456cf2d9096c4ac7e8
hamzoalbo <84898985+hamzoalbo@users.noreply.github.com>
2022-02-25 03:14:46 +0100

    finale Version

--- CH9.6.3.c
+++ CH9.6.3.c
@@ -5,7 +5,7 @@
  * Author: Hamzenis Kryeziu
  * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
  * -----
- * Last Modified: 2022-02-25, 2:43:34 am
+ * Last Modified: 2022-02-25, 3:14:36 am
  * Modified By: Hamzenis Kryeziu
  * -----
  * Copyright (c) 2022
@@ -15,11 +15,10 @@
  * HISTORY:
  * Date              		By		Comments
  * ------------------		----	----------------------------------------------------------
+ * 2022-02-25, 3:11:09 am	H.K.	added printArr()
  * 2022-02-25, 2:25:27 am	H.K.	added comments
  * 2022-01-24, 2:08:17 am	H.K.	-start-
  */
-
- // noch nicht fertig
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,16 +35,16 @@
 *   rows oder collums mitgegeben werden. Aus Konvetion übergibt man
 *   jedoch die collums.
 */
-void printArr(char ptrArrName[][MAX]){
+void printArr(char ptrArrName[][MAX]) {
     for (int i = 0; i < 6; i++) {
-        printf("Sort Namen:\t%d. %s\n", i + 1 , ptrArrName[i]);
+        printf("Sort Namen:\t%d. %s\n", i + 1, ptrArrName[i]);
     }
 }
 
 int main() {
 
     // Ein Array of Strings zum speichern der Namen. 
-    char arrName[STRINGS][MAX] = {      {"Peter"},
+    char arrName[STRINGS][MAX] = { {"Peter"},
                                         {"Paul"},
                                         {"Mary"},
                                         {"Puff"},
@@ -55,35 +54,37 @@ int main() {
     char temp[MAX];
 
     /*
-    *  Variable damit die Länge des Arrays errechnet. 
+    *  Variable damit die Länge des Arrays errechn 
     */
     int arrSize = sizeof(arrName) / sizeof(arrName[0]);
 
     /*
-    *  Verschachtelte For-Schleife. 
+    *  Verschachtelte For-Schleife.
     *  Zur Visalisierung des Sortierprozesses wird
-    *  cmpVal & die Reihenfolge der jetztigen Namen bei jedem 
-    *  Vergleich geprintet. Damit sieht man das sortieren und 
+    *  cmpVal & die Reihenfolge der jetztigen Namen bei jedem
+    *  Vergleich geprintet. Damit sieht man das sortieren und
     *  den Stand der Namen.
     */
-    for (int  i= 0; i < 6 - 1; i++) { 
-        printf("\ni: %d", i+1);
+    printArr(arrName);
+    for (int i = 0; i < 6 - 1; i++) {
+        printf("\ni: %d", i + 1);
 
         for (int j = 0; j < 6 - 1; j++) {
             cmpVal = strcmp(arrName[j], arrName[j + 1]);
-            printf("\t\tj: %d. %d\n", j+1, cmpVal);
-            
+            printf("\tj: %d.  %d  \t", j + 1, cmpVal);
+
             if (cmpVal > 0) {
                 strcpy(temp, arrName[j + 1]);
                 strcpy(arrName[j + 1], arrName[j]);
                 strcpy(arrName[j], temp);
             }
+            printf("%d. %s\n", j + 1, arrName[j]);
         }
-        
+        printf("\t\t\t6. %s\n", arrName[5]);
     }
 
     printf("\n");
     printArr(arrName);
-    
+
     return 0;
 }

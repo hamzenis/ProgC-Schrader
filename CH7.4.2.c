308d9123013d89471ff41c176bfa40ce909f46cd
hamzoalbo <84898985+hamzoalbo@users.noreply.github.com>
2022-02-25 13:38:17 +0100

    Create CH7.4.2.c

---
+++ CH7.4.2.c
@@ -0,0 +1,36 @@
+/*
+ * File: CH7.4.2.c
+ * Project: Einführung in C - Schrader
+ * Created Date: 2022-02-25, 1:34:50 pm
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-02-25, 1:38:03 pm
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022
+ *
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-02-25, 1:34:50 pm	H.K.	-start-
+ */
+
+#include <stdio.h>
+
+ // noch nicht fertig
+
+int main() {
+    
+    int i1[10];
+    int temp;
+    int iSum[10];
+
+    
+
+
+
+    return 0;
+}

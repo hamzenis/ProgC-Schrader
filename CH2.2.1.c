1a0ad76b3bb2544854b3b5455616f4d025b17f70
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-02-28 19:12:04 +0100

    finale Version
    Co-Authored-By: Ibrahim Enes Hayber <100522586+whis-uas@users.noreply.github.com>

--- CH2.2.1.c
+++ CH2.2.1.c
@@ -4,7 +4,7 @@
  * Created Date: 2022-02-26, 11:45:39 pm
  * Author: Ibrahim Hayber
  * -----
- * Last Modified: 2022-02-28, 1:06:25 am
+ * Last Modified: 2022-02-28, 7:11:33 pm
  * Modified By: Hamzenis Kryeziu
  * -----
  * Copyright (c) 2022 
@@ -18,11 +18,11 @@
  * 2022-02-26, 11:45:39 pm	I.H.	-start-
  */
 
- /*
- Schreiben sie das Programm "helloWorld.c¨aus dem vorherigen Abschnitt 2.1.3 so um,
- dass es seinen Quelltext am Bildschirm ausgibt. Dieses ist mit dem bisher erlernten
- printf und den Escape-Sequenzen zu loesen.
- */
+/*
+Schreiben sie das Programm "helloWorld.c¨aus dem vorherigen Abschnitt 2.1.3 so um,
+dass es seinen Quelltext am Bildschirm ausgibt. Dieses ist mit dem bisher erlernten
+printf und den Escape-Sequenzen zu loesen.
+*/
 
 #include <stdio.h>
 

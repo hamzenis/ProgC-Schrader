80d05a30c136cbd3d3885126f8d14a2b54039b9a
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-04-19 15:41:14 +0200

    Create Aufgabe4.c

---
+++ C WS2021:22/Aufgabe4.c
@@ -0,0 +1,24 @@
+/*
+ * File: Aufgabe4.c
+ * Project: Einführung in C - Schrader
+ * Created Date: 2022-04-06, 5:24:07 pm
+ * Author: Hamzenis Kryeziu
+ * E-Mail: hamzenis.kryeziu@stud.fra-uas.de
+ * -----
+ * Last Modified: 2022-04-06, 5:24:47 pm
+ * Modified By: Hamzenis Kryeziu
+ * -----
+ * Copyright (c) 2022 
+ * 
+ * Free for use
+ * -----
+ * HISTORY:
+ * Date              		By		Comments
+ * ------------------		----	----------------------------------------------------------
+ * 2022-04-06, 5:24:07 pm	H.K.	-start-
+ */
+
+//  noch in Bearbeitung
+
+#include <stdio.h>
+#include <stdlib.h>
